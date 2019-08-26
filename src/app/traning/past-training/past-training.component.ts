f8d5e029e01585a4eebeb3d90f59b40e7c3acf14
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-08-27 00:32:34 +0930

    data table

--- src/app/traning/past-training/past-training.component.ts
+++ src/app/traning/past-training/past-training.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
 
-  constructor() { }
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private traininService: TrainingService) {}
 
   ngOnInit() {
+    this.dataSource.data = this.traininService.getCompletedOrCancelledExercises();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  doFilter(searchText) {
+    this.dataSource.filter = searchText.trim().toLowerCase();
+  }
 }
